4767b53772c216dd68d8840e901a717f70c3bc3a
farid <faridramadhan59@gmail.com>
2025-07-12 13:11:56 +0700

    fix: replace return error with throw error

--- tasks/farid/printTable.js
+++ tasks/farid/printTable.js
@@ -1,7 +1,6 @@
 const printTable = (numbers) => {
     if (typeof numbers !== "number" || numbers <= 0) {
-        console.log("Parameter harus bertipe data nomor dan harus nomor positif");
-        return;
+        throw new Error("Parameter harus bertipe data nomor dan harus nomor positif");
     }
 
     for (let i = 1; i <= 10; i++) {
