aff8f9cf47a25707f7c68ea1dc99211551f89111
farid <faridramadhan59@gmail.com>
2025-07-12 12:47:46 +0700

    feat: add set of features

---
+++ tasks/farid/index.js
@@ -0,0 +1,15 @@
+const triangle1st = require("./triangle1st");
+const countArr = require("./countArr");
+const triangle2nd = require("./triangle2nd");
+const processNumbers = require("./processNumbers");
+const printTable = require("./printTable");
+const range = require("./range");
+
+module.exports = {
+    triangle1st:triangle1st,
+    countArr:countArr,
+    triangle2nd:triangle2nd,
+    processNumbers:processNumbers,
+    printTable:printTable,
+    range:range
+};
